ff4a954488a4b67eb69ebc5c93eff98a49b02802
malejs <34074178+malejs@users.noreply.github.com>
2019-10-22 11:51:10 +0300

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1q9zYVkpoLkV1ExaLX3e4sWsxpWu5mF64LuPE2iNWB6Q/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1kFS_YS3VluCpJqxoycbIc8Gu7c1k3BRZRtumH_-G-58/edit#gid=0';
